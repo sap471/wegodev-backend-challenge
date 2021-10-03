ff5a8f3d6dc0b9b93cfe3921c85e9caf129031cd
ntzb <ntbz@htdocs.xyz>
2021-10-04 03:12:16 +0700

    heroku postgres ssl fix 2

--- app/database.ts
+++ app/database.ts
@@ -13,6 +13,9 @@ const getOptions = async (): Promise<ConnectionOptions> => {
     synchronize: true,
     logging: false,
     entities: ["dist/entities/*.*"],
+    ssl: {
+      rejectUnauthorized: false,
+    },
   };
 
   if (process.env.DATABASE_URL) {
